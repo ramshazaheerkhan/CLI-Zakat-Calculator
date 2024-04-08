7f16bec2c037d65ce1a92fc59074bf0ea8d9582a
ramshazaheerkhan <162893622+ramshazaheerkhan@users.noreply.github.com>
2024-04-07 18:42:36 -0700

    Add files via upload

---
+++ zakat.js
@@ -0,0 +1,21 @@
+#! /ussr/bin/env node
+import inquirer from "inquirer";
+let zakat = 2.5;
+let goldvalueInTola = 234800; // Approx
+let goldweightInTola = 7.5;
+const answer = await inquirer.prompt([
+    {
+        name: "Gold",
+        message: "Enter Gold Weight:",
+        type: "number",
+    }
+]);
+let goldweight = answer.Gold;
+let goldweightInTolaEntered = goldweight / 11.6638038; // Conversion from grams to tola
+if (goldweightInTolaEntered >= goldweightInTola) {
+    let calculate_zakat = (goldweightInTolaEntered * goldvalueInTola) * (zakat / 100);
+    console.log(`The total calculated value of gold weighing ${goldweight} grams is ${calculate_zakat}`);
+}
+else {
+    console.log("Zakat is not applicable");
+}
